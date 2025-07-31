b0074c032c6889f9ab703e93c064b746145d1d8a
spark <fabricio.jacob@outlook.com>
2025-07-31 08:50:49 -0300

    feat: init project.

---
+++ src/main/java/com/github/fabriciolfj/payment_card/PaymentCardApplication.java
@@ -0,0 +1,13 @@
+package com.github.fabriciolfj.payment_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentCardApplication.class, args);
+	}
+
+}
